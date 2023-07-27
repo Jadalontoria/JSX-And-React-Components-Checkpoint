fd3371fa83042facec7db6dceb5775db5bbddc39
Jadalontoria <jadalontoriaa@gmail.com>
2023-07-27 14:20:46 +0100

    First Commit

---
+++ src/components/profile/Address.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <div>
+        <h1>My Address is : 1a Gabby Adesoun Street, Lekki Phase 1, Lagos</h1>
+    </div>
+  )
+}
+
+export default Address
